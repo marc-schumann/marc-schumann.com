5b994e73391c33c2bf1e9eb671969759989d5b3a
unknown <MarcSchumann@MarcSchumann-NB.(none)>
2012-10-19 14:23:03 +0200

    initial porject version

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Testtitel</title>
+	</head>
+	<body>
+		<div>
+			Das ist eine Testseite von <?php echo $_SERVER['HTTP_HOST']?>
+		</div>
+	</body>
+</html>
